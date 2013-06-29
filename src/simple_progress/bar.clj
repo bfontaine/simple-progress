bc7f542f421958a51c3fb8377397eae7e0c29d96
Baptiste Fontaine <batifon@yahoo.fr>
2013-06-29 16:37:21 +0200

    mk-progress-bar added.

--- src/simple_progress/bar.clj
+++ src/simple_progress/bar.clj
@@ -1,1 +1,29 @@
 (ns simple-progress.bar)
+
+(defn bar->string
+  [curr max]
+
+  ;; TODO
+  (str curr "/" max))
+
+(defn mk-progress-bar
+  "Return a function which describe a progress bar"
+  [& {:keys [max percents]}]
+  (let [max  (if (or (< 0 max) (not max)) 100 max)
+        percents (or percents (= max 100))
+        curr (atom 0)]
+
+    (fn bar [action & options]
+
+      (let [noprint (options :noprint)
+            opt     (first options)
+            times   (or (and (number? opt) opt) 1)]
+
+        (case action
+          :inc (swap! curr (partial + count))
+          :dec (swap! curr (partial - count))))
+
+      (when (not noprint)
+        (print (str "\r" (bar->string @curr max))))
+      
+      @curr)))
